4fd2a0184545b6c4366b38f911eb8b65785f78dd
TanujaBG <tanuj@DESKTOP-NC51C1K>
2026-02-01 22:17:33 -0800

    Added checkout page into react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,14 @@
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import {Routes, Route} from 'react-router'
+import { CheckoutPage } from './pages/CheckoutPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="checkout" element={<h1>Checkout Page</h1>} />
+        <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<h1>Orders Page</h1>} />
         <Route path="tracking" element={<h1>Tracking Page</h1>} />
       </Routes>
