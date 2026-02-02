ec5c306d631100129824a6ef0a426268c656a456
TanujaBG <tanuj@DESKTOP-NC51C1K>
2026-02-01 22:08:07 -0800

    Created multiple pages using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,17 @@
 import { HomePage } from './pages/HomePage'
 import './App.css'
+import {Routes, Route} from 'react-router'
 
 function App() {
   return (
-    <HomePage />
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<h1>Checkout Page</h1>} />
+        <Route path="orders" element={<h1>Orders Page</h1>} />
+        <Route path="tracking" element={<h1>Tracking Page</h1>} />
+      </Routes>
+    </>
   );
 }
 
