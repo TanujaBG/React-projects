e53b8b710e81c60d6cc09bde1686eed9824d9dfb
TanujaBG <tanuj@DESKTOP-NC51C1K>
2026-02-02 13:30:16 -0800

    Excersice till 6d

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import {Routes, Route} from 'react-router'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/Checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 
