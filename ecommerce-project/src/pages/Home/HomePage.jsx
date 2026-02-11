b896557ab13cb84df2abab57effff27e1a0593e1
TanujaBG <tanuj@DESKTOP-NC51C1K>
2026-02-11 12:24:36 -0800

    Create add to cart

--- ecommerce-project/src/pages/Home/HomePage.jsx
+++ ecommerce-project/src/pages/Home/HomePage.jsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/Header.jsx";
 import { useEffect, useState } from "react";
 import { ProductsGrid } from "./ProductsGrid";
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -34,7 +34,7 @@ export function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart}/>
       </div>
     </>
   );
