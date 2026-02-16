b0e5b6b4627c6622a490d25f6d524b22073dc377
TanujaBG <tanuj@DESKTOP-NC51C1K>
2026-02-15 16:51:47 -0800

    Used beforeEAch to remove duplication

--- ecommerce-project/src/pages/Home/Product.test.jsx
+++ ecommerce-project/src/pages/Home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Product } from "./Product";
 import userEvent from "@testing-library/user-event"; // this lets us similate events like onclick
@@ -7,8 +7,10 @@ import axios from "axios";
 vi.mock("axios"); // this mocks all axio calls
 
 describe("Product Component", () => {
-  it("displays the product details correctly", () => {
-    const product = {
+  let product, loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,8 +22,10 @@ describe("Product Component", () => {
       keywords: ["socks", "sports", "apparel"],
     };
 
-    const loadCart = vi.fn();
+    loadCart = vi.fn();
+  });
 
+  it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(
@@ -41,20 +45,6 @@ describe("Product Component", () => {
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
-
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
